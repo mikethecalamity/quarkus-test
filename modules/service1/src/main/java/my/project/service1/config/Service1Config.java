49700f00497fac4c396e757f7bd8a8c82ebe80c0
Mike Weber <michael.w.weber@lmco.com>
2021-07-14 13:04:01 -0400

    Fixed config class

--- modules/service1/src/main/java/my/project/service1/config/Service1Config.java
+++ modules/service1/src/main/java/my/project/service1/config/Service1Config.java
@@ -5,13 +5,5 @@ import io.smallrye.config.ConfigMapping;
 @ConfigMapping
 public class Service1Config {
 
-    private String name;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(final String name) {
-        this.name = name;
-    }
+    String name();
 }
