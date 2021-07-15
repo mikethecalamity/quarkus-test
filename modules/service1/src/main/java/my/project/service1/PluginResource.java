0635de65007c65ebb89eacb222020a21d75121e8
Mike Weber <michael.w.weber@lmco.com>
2021-07-15 13:15:32 -0400

    Updated config usage

--- modules/service1/src/main/java/my/project/service1/PluginResource.java
+++ modules/service1/src/main/java/my/project/service1/PluginResource.java
@@ -1,5 +1,8 @@
 package my.project.service1;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,14 +19,14 @@ public class PluginResource {
     @ConfigProperty(name = "app.other")
     String other;
 
-    @ConfigMapping(prefix = "app.test")
-    Service1Config config;
+    @ConfigMapping(prefix = "app.items")
+    List<Service1Config> config;
 
     @GET
-    @Path("name")
+    @Path("names")
     @Produces(MediaType.TEXT_PLAIN)
-    public String name() {
-        return this.config.name();
+    public List<String> names() {
+        return this.config.stream().map(Service1Config::name).collect(Collectors.toList());
     }
 
     @GET
--- modules/service1/src/main/java/my/project/service1/config/Service1Config.java
+++ modules/service1/src/main/java/my/project/service1/config/Service1Config.java
@@ -3,4 +3,5 @@ package my.project.service1.config;
 public interface Service1Config {
 
     String name();
+    int num();
 }
