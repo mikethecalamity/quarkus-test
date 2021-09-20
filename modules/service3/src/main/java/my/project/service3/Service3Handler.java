2dadab120347e2cfc3c9fecc2a1f8605b02ad1e1
mweber <mweber03@villanova.edu>
2021-09-20 14:27:24 -0400

    Another config mapping error

--- modules/service3/src/main/java/my/project/service3/Service3Handler.java
+++ modules/service3/src/main/java/my/project/service3/Service3Handler.java
@@ -1,11 +1,14 @@
 package my.project.service3;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
 @Dependent
 public class Service3Handler {
@@ -15,10 +18,10 @@ public class Service3Handler {
     private final String value2;
 
     @Inject
-    public Service3Handler(@ConfigProperty(name = "app.other") final String value0, final Config config) {
-        this.value0 = value0;
-        this.value1 = String.format("%s-%s", config.myConfig().name(), config.myConfig().num());
-        this.value2 = String.format("%s-%s", config.theirConfig().name(), config.theirConfig().num());
+    public Service3Handler(final JmsConfig config) {
+        this.value0 = Boolean.toString(config.ignoreAnnotatedListeners());
+        this.value1 = config.sessions().stream().flatMap(Collection::stream).findFirst().map(JmsSessionConfig::name).map(Optional::get).orElse("none1");
+        this.value2 = config.listeners().stream().flatMap(Collection::stream).findFirst().map(JmsListenerConfig::session).orElse("none2");
     }
 
     public String getValue0() {
@@ -33,14 +36,112 @@ public class Service3Handler {
         return this.value2;
     }
 
-    @ConfigMapping(prefix = "app.config")
-    interface Config {
-        InnerConfig myConfig();
-        InnerConfig theirConfig();
+    /**
+     * Configuration for JMS
+     */
+    @ConfigMapping(prefix = "app.jms")
+    interface JmsConfig {
+
+        /**
+         * @return boolean indicating if listeners that are annotated and not in the configuration should be ignored
+         */
+        @WithDefault("true")
+        boolean ignoreAnnotatedListeners();
+
+        /**
+         * @return the JMS sessions
+         */
+        Optional<List<JmsSessionConfig>> sessions();
+
+        /**
+         * @return the JMS listeners
+         */
+        Optional<List<JmsListenerConfig>> listeners();
     }
 
-    interface InnerConfig {
-        String name();
-        int num();
+    /**
+     * Configuration for JMS session
+     */
+    interface JmsSessionConfig {
+
+        /**
+         * @return the session name
+         */
+        Optional<String> name();
+
+        /**
+         * @return the session URI
+         */
+        Optional<String> uri();
+
+        /**
+         * @return the session username
+         */
+        Optional<String> username();
+
+        /**
+         * @return the session password
+         */
+        Optional<String> password();
+
+        /**
+         * @return the session acknowledgement option
+         */
+        Optional<Integer> ackOption();
+
+        /**
+         * @return the session connection factory fully qualified class name
+         */
+        Optional<String> connectionFactory();
     }
+
+    /**
+     * Configuration for JMS listener
+     */
+    interface JmsListenerConfig {
+
+        /**
+         * @return flag indicating if this listener is disabled or not
+         */
+        @WithDefault("false")
+        boolean disabled();
+
+        /**
+         * @return flag indicating if annotations should be ignored for this listener
+         */
+        @WithDefault("true")
+        boolean ignoreAnnotated();
+
+        /**
+         * @return the listener fully qualified class name
+         */
+        String listenerClass();
+
+        /**
+         * @return the listener session name
+         */
+        String session();
+
+        /**
+         * @return the listener pool size
+         */
+        Optional<Integer> poolSize();
+
+        /**
+         * @return the listener flag indicating if is asynchronous
+         */
+        Optional<Boolean> async();
+
+        /**
+         * @return the listener queues
+         */
+        Optional<List<String>> queues();
+
+        /**
+         * @return the listener topics
+         */
+        Optional<List<String>> topics();
+    }
+
+
 }
