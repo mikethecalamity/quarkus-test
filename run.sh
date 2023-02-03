b209ea25a6a2585af7ed8161840609b6dcf06834
mweber <mweber03@villanova.edu>
2023-02-03 18:33:20 -0500

    Clean up

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 service=$1
-./gradlew $service:quarkusBuild
-sudo docker build -t $service -f modules/$service/Dockerfile .
-sudo docker run -it -p 8080:8080 $service
 
+./gradlew $service:quarkusBuild
+
+if [ $? -eq 0 ]; then
+  sudo docker build -t $service -f modules/$service/Dockerfile .
+   
+  if [ $? -eq 0 ]; then
+    sudo docker run -it -p 8080:8080 $service
+  fi
+fi
